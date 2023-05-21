dffcff16cde1d0ab0ceaf28ea3421c2ef6fd3789
karaba <krav@192.168.1.9>
2023-05-21 19:37:46 -0300

    enemigo Agustin

---
+++ src/Library/Items/Golemite.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace Library
+{
+    public class Golemite : Items
+    {
+        public Golemite()
+        {
+            this.damage = 60;
+            this.defense = 15;
+        }
+    }
+}
