e5afa95e94a2ef6d7e2804b873118019017a9245
IgnacioMartinez01 <igmartinezpintosgmail.com>
2023-05-26 16:20:29 -0300

    Elemento creado

---
+++ src/Library/Items/BalttleAxe.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+namespace Library
+{
+    public class BattleAxe : IAttackItem, IDefenseItem
+    {
+        public double damage { get ; set ;}
+        public double defense {get ; set;}
+        public BattleAxe(string name)
+        {
+            this.damage = 65;
+            this.defense = 20;
+            
+
+            
+
+            
+        }
+    }
+}
