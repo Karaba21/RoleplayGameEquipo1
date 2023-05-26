924f209d9d733832f2bffa3f683d85997741c66b
Alfonso Gesto <alfonso.gesto@correo.ucu.edu.uy>
2023-05-26 16:17:38 -0300

    Class Troll y test hechos

---
+++ src/Test/Library.Test/TrollTest.cs
@@ -0,0 +1,58 @@
+using System;
+using Library;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Library.Test
+{
+    public class TrollTest
+    {
+        //Esta clase tiene  test realizados.
+        private Dwarf dwarf;
+        private Troll troll;
+
+        [SetUp]
+        public void SetUp()
+        {
+            dwarf = new Dwarf("Monty"); // defense = 40
+            troll = new Troll("Georgie"); // defense = 
+        }
+
+        [Test]
+        public void GetTotalDamageTest()
+        {
+            // Verifico que la cuenta de totaldamage funcione
+            double expected = 110;
+
+            Assert.That(expected, Is.EqualTo(troll.totalDamage));
+        }
+
+        [Test]
+        public void GetTotalDefenseTest()
+        {
+            // Verifico que la cuenta de totaldefense funcione
+            double expected = 60;
+
+            Assert.That(expected, Is.EqualTo(troll.totalDefense));
+        }
+        [Test]
+        public void UseStick() // damage = 55
+        {
+            // Testeo que funcione el ataque con el palo.
+            double expected = 985;
+            troll.UseStick(dwarf);
+
+            Assert.That(expected, Is.EqualTo(dwarf.health));
+        }
+        
+        [Test]
+        public void UseAllStrengthTest()
+        {
+            // Testeo que funcione el método UseAllStrength()
+            double expected = 930;
+            troll.UseAllStrength(dwarf);
+
+            Assert.That(expected, Is.EqualTo(dwarf.health));
+        }
+    }
+}
