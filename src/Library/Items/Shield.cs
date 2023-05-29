d14af6edd82f1bc44cf07bd45b3ce91b3945359a
agustinabenia25 <agustina.benia@correo.ucu.edu.uy>
2023-05-29 21:16:21 +0100

    Correciones

--- src/Library/Items/Shield.cs
+++ src/Library/Items/Shield.cs
@@ -1,14 +1,12 @@
 namespace Library
 {
-    public class Shield : IAttackItem, IDefenseItem
+    public class Shield : IDefenseItem
     {
         public string name { get ; set ;}
-        public double damage { get ; set ;}
         public double defense { get ; set ;}
         public Shield()
         {
             this.name = "Shield";
-            this.damage = 0;
             this.defense = 50;
         }
     }
--- src/Library/Items/WhirlWind.cs
+++ src/Library/Items/WhirlWind.cs
@@ -1,15 +1,13 @@
 namespace Library
 {
-    public class WhirlWind : IAttackItem, IDefenseItem
+    public class WhirlWind : IAttackItem
     {
         public string name { get ; set ;}
         public double damage { get ; set ;}
-        public double defense { get ; set ;}
         public WhirlWind()
         {
             this.name = "Whirl Wind";
             this.damage = 90;
-            this.defense = 0;
         }
     }
 }
